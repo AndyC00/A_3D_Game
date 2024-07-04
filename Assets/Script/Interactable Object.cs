ce38f27a87f6fc72011308144ebdc8fd2c90cde9
AndyC00 <chenjinzhi96@gmail.com>
2024-07-04 16:01:12 +1200

    04/07
    tuning interaction with objects

--- Assets/Script/Interactable Object.cs
+++ Assets/Script/Interactable Object.cs
@@ -5,13 +5,30 @@ using UnityEngine.AI;
 
 public class InteractableObject : MonoBehaviour
 {
+    private NavMeshAgent playerAgent;
+    private bool haveInteracted = false;
     public void OnClick(NavMeshAgent playerAgent)
     {
+        this.playerAgent = playerAgent;
+
         playerAgent.stoppingDistance = 2;
 
         playerAgent.SetDestination(transform.position);
 
-        Interact();
+        haveInteracted = false;
+        //Interact();
+    }
+
+    private void Update()
+    {
+        if (playerAgent != null && playerAgent.pathPending == false && haveInteracted == false)
+        {
+            if (playerAgent.remainingDistance <= 2)
+            {
+                Interact();
+                haveInteracted |= true;
+            }
+        }
     }
 
     protected virtual void Interact()
