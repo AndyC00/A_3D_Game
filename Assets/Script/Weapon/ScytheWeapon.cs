83bf331abf373d71907f25c163503564e2e54c28
AndyC00 <chenjinzhi96@gmail.com>
2024-07-27 13:16:35 +1200

    attack detection

--- Assets/Script/Weapon/ScytheWeapon.cs
+++ Assets/Script/Weapon/ScytheWeapon.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class ScytheWeapon : Weapon
 {
+    public const string ANIM_PARM_ISATTACK = "IsAttack";
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Press "J" to attack
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Attack();
+        }
+    }
+
     public override void Attack()
     {
-        base.Attack();  
+        anim.SetTrigger(ANIM_PARM_ISATTACK); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //todo: enemy
+            print("Trigger with " + other.name);
+        }
     }
 }
