a89b4ae1aa1e1750c1208bb63b746ed97ff09d70
AndyC00 <chenjinzhi96@gmail.com>
2024-08-27 17:47:55 +1200

    finished the scriptobject

--- Assets/Script/SO/ItemScriptObject.cs
+++ Assets/Script/SO/ItemScriptObject.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class ItemSO : ScriptableObject
 {
-    public string Name { get; set; }
-    public ItemType ItemType { get; set; }
-
+    public string name;
+    public ItemType itemType;
+    public string description;
+    public List<ItemProperty> propertyList;
+    public Sprite icon;
+    public GameObject prefab;
 }
 
 public enum ItemType
 { 
     Weapon,
     Consumable
+}
+
+[Serializable]
+public class ItemProperty
+{
+    public ItemPropertyType propertytype;
+    public int value;
+}
+
+public enum ItemPropertyType
+{ 
+    HPValue,
+    EnergyValue,
+    MentalValue,
+    SpeedValue,
+    AttackValue
 }
